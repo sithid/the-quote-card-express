f428d06f18be34bb9d961fc6fa1e3bbfc5bf7e80
James Glosser <demonicurges05@gmail.com>
2025-06-10 00:57:25 -0400

    background image wasnt covering entire card, issue fixed

--- public/js/index.js
+++ public/js/index.js
@@ -6,17 +6,21 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
+    const client_id = "HIDDEN_API_KEY";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
         const response = await fetch(endpoint);
-        const returnedData = await response.json()
-        console.log(returnedData)
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
+getRandomImage();
 /*
 const quotes = [
   {
@@ -69,4 +73,4 @@ function loopThroughQuotes() {
 }
 
 setTimeout(loopThroughQuotes, 3000);
-/*
+*/
