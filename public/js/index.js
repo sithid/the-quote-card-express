2a253bf283ea6311aab21f2e2ee7f0a5cff705ac
James Glosser <demonicurges05@gmail.com>
2025-06-10 00:16:48 -0400

    quotes card part 3 finished

---
+++ public/js/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const elements = {
+  quote: document.getElementById("quote"),
+  author: document.getElementById("author"),
+};
+
+const quotes = [
+  {
+    quote: "I think, therefore I am.",
+    author: "René Descartes",
+  },
+
+  {
+    quote: "Doh!",
+    author: "Homer Simpson",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote: "Get busy living or get busy dying.",
+    author: "Stephen King",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates",
+  },
+  {
+    quote: "Pure mathemetics is, in its way, the poetry of logical ideas.",
+    author: "Albert Einstein",
+  },
+];
+
+function loopThroughQuotes() {
+  let quoteIndex = 0;
+  setInterval(() => {
+    if (quoteIndex < quotes.length) {
+      elements.quote.textContent = quotes[quoteIndex].quote;
+      elements.author.textContent = quotes[quoteIndex].author;
+      quoteIndex++;
+    } else {
+      quoteIndex = 0;
+    }
+  }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
