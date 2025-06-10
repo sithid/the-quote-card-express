3512006a77fb0e636a7e12d8c31061174e5dda27
James Glosser <demonicurges05@gmail.com>
2025-06-10 14:40:20 -0400

    getRandomImage API endpoint implemented

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,11 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = "HIDDEN_API_KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
         imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
@@ -21,6 +20,7 @@ async function getRandomImage() {
 }
 
 getRandomImage();
+
 /*
 const quotes = [
   {
